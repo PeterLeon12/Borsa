0be9ed32e3d08b8b2a5f3ff720e211280fe88900
Peter <timis.petre51@gmail.com>
2024-09-27 17:27:38 +0300

    Updated 5 added Crud capabilities

---
+++ Helpers/CircleImage.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        ZStack {
+            // Background blurred image
+            image
+                .resizable()
+                .blur(radius: 10) // Apply a blur effect
+                .ignoresSafeArea()
+                .frame(width: 420, height: 360)
+
+            // Foreground circular image
+            image
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth: 1)
+                }
+                .shadow(radius: 7)
+                .frame(width: 250, height: 250) // Set the size of the circular image
+        }
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
