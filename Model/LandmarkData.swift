1817beff40099223812dc9609c79661b798414e7
Peter <timis.petre51@gmail.com>
2024-10-09 00:24:55 +0300

    Update 7 Crud capabilities and  Access the shared data

---
+++ Model/LandmarkData.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkData.swift
+//  LandMarks
+//
+//  Created by Timis Petre Leon on 08.10.2024.
+//
+
+import SwiftUI
+
+struct LandmarkData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkData()
+}
